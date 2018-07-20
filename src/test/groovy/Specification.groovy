dc2cb7d3054d0ec6f1cd5c5be635e7589e958262
Pablo Pedraza <Pablo@WKS783L.sofrecom.local>
2018-07-20 14:25:07 -0300

    probando spock

--- src/test/groovy/Specification.groovy
+++ src/test/groovy/Specification.groovy
@@ -1,41 +1,43 @@
-import groovyx.net.http.RESTClient
+import spock.lang.Shared
 import spock.lang.Specification
-import spock.lang.Unroll
-
-class RestSpecification extends Specification {
-
-    RESTClient restClient = new RESTClient("http://api.openweathermap.org")
-
-    def 'Check if the weather in Eindhoven can be found'() {
-        given:
-        String city = "Eindhoven,nl"
-
-        when:
-        def response = restClient.get( path: '/data/2.5/weather',
-                query: ['q' : city])
-
-        then:
-        response.status == 200
-
-        and:
-        response.responseData.name == "Eindhoven"
+ 
+class ExampleSpec extends Specification {
+ 
+    def message = "Hello world!"
+ 
+    def setup() {
+        println "Setup"
     }
-
-    @Unroll("Check #city matches #expectedResult")
-    def 'Check if we can find multiple cities'() {
-        when:
-        def response = restClient.get( path: '/data/2.5/weather', query: ['q' : city])
-
-        then:
-        assert response.status == 200
-
-        and:
-        response.responseData.name == expectedResult
-
-        where:
-        city            | expectedResult
-        "Eindhoven,nl"  | "Eindhoven"
-        "Amsterdam,nl"  | "Amsterdam"
-        "Brussels,be"   | "Brussels"
+ 
+    def cleanup() {
+        println "Clean up"
+    }
+ 
+    def setupSpec() {
+        println "Setup specification"
+    }
+ 
+    def cleanupSpec() {
+        println "Clean up specification"
+    }
+ 
+    def "first feature method"() {
+        println "First feature method"
+ 
+        when: "Message is transformed into lowercase"
+        message = message.toLowerCase()
+ 
+        then: "Should transform message into lowercase"
+        message == "hello world!"
+    }
+ 
+    def "second feature method"() {
+        println "Second feature method"
+ 
+        when: "Message is transformed into uppercase"
+        message = message.toUpperCase()
+ 
+        then: "Should transform message into uppercase"
+        message == "HELLO WORLD!"
     }
 }
