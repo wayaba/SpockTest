a5e76b709a6ab593ebc7a2a94b54833f96f37226
Pablo Pedraza <Pablo@WKS783L.sofrecom.local>
2018-07-20 14:52:22 -0300

    probando spock

--- src/test/groovy/Specification.groovy
+++ src/test/groovy/Specification.groovy
@@ -1,9 +1,12 @@
 import spock.lang.*
+import groovyx.net.http.RESTClient
 
 // Hit 'Run Script' below
 class MyFirstSpec extends Specification {
-  def "let's try this!"() {
-    expect:
-    Math.max(1, 2) == 3
-  }
+	
+	RESTClient restClient = new RESTClient("http://192.168.99.100:7801", JSON)
+	def "let's try this!"() {
+		expect:
+		Math.max(1, 2) == 3
+	}
 }
