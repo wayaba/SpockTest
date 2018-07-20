25d8f393a766a82a451277ef5791ea617af6ed41
Pablo Pedraza <Pablo@WKS783L.sofrecom.local>
2018-07-20 15:54:44 -0300

    probando spock

--- ApiMascotas/test/groovy/Specification.groovy
+++ ApiMascotas/test/groovy/Specification.groovy
@@ -1,39 +1,32 @@
+import spock.lang.*
 import groovyx.net.http.RESTClient
-import spock.lang.Specification
 
-class RestSpecification extends Specification {
+// Hit 'Run Script' below
+class MyFirstSpec extends Specification {
+	
 
     RESTClient restClient = new RESTClient("http://192.168.99.100:7801")
-
-    def 'Check if the weather in Eindhoven can be found'() {
-        given:
+	
+	def 'Checkeo getPetsById'() {
+		given:
         String petid = "2"
-
-        when:
-        def response = restClient.get( path: '/v2/pet/getPetsById',
-                query: ['petId' : petid])
-
-        then:
-        response.status == 200
-
-        and:
-        response.responseData.name == "Eindhoven"
-    }
-
-    def 'Check if we can find multiple cities'() {
-        when:
+		
+		when:
         def response = restClient.get( path: '/v2/pet/getPetsById', query: ['petId' : petid])
 
-        then:
-        assert response.status == 200
+		then:
+            with (response) {
+                status == 200
+                
+            }            
+    }
+	
+	def 'Checkeo get all'() {
+		
+		when:
+        def response = restClient.get(path: '/v2/pet')
 
-        and:
-        response.responseData.name == expectedResult
-
-        where:
-        petid            | expectedResult
-        "2"  | "Fido"
-        "3"  | "Dico"
-        "4"   | "Brussels"
+		then:
+            assert response.status == 200
     }
 }
