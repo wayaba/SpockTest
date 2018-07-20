b7cf679fb7ffbe06c809678f338d65a4bf9ab36e
Pablo Pedraza <Pablo@WKS783L.sofrecom.local>
2018-07-20 14:45:18 -0300

    probando spock

--- src/test/groovy/Specification.groovy
+++ src/test/groovy/Specification.groovy
@@ -1,43 +1,9 @@
-import spock.lang.Shared
-import spock.lang.Specification
- 
-class ExampleSpec extends Specification {
- 
-    def message = "Hello world!"
- 
-    def setup() {
-        println "Setup"
-    }
- 
-    def cleanup() {
-        println "Clean up"
-    }
- 
-    def setupSpec() {
-        println "Setup specification"
-    }
- 
-    def cleanupSpec() {
-        println "Clean up specification"
-    }
- 
-    def "first feature method"() {
-        println "First feature method"
- 
-        when: "Message is transformed into lowercase"
-        message = message.toLowerCase()
- 
-        then: "Should transform message into lowercase"
-        message == "hello world!"
-    }
- 
-    def "second feature method"() {
-        println "Second feature method"
- 
-        when: "Message is transformed into uppercase"
-        message = message.toUpperCase()
- 
-        then: "Should transform message into uppercase"
-        message == "HELLO WORLD!"
-    }
+import spock.lang.*
+
+// Hit 'Run Script' below
+class MyFirstSpec extends Specification {
+  def "let's try this!"() {
+    expect:
+    Math.max(1, 2) == 3
+  }
 }
